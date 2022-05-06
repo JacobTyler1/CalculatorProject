94f448c6a75ad012315a4aaedbf5132df34d6eed
Jacob Tyler <jacob.tyler@uvm.edu>
2022-05-06 15:02:06 -0700

    Added algebra tests

--- calc_tests.py
+++ calc_tests.py
@@ -12,3 +12,17 @@ for i in range(len(test_expressions)):
     assert result == answers_expressions[i], f"Expected {answers_expressions[i]}, got {result}"
     print("PASS")
 
+"""
+Testing algebra solver
+"""
+
+test_formulas = ["2x*2=4", "2*(2+2)*2x=32", "2x+2y=20"]
+answer_formulas = [(1, 0), (2, 0), (10, 0)]
+
+for i in range(len(test_formulas)):
+    print("Test " + str(i) + ": " + test_formulas[i])
+    result = solve(test_formulas[i])
+    # Test will round to nearest int and compare with expected int values
+    assert (int(float(result[0])), int(float(result[1]))) == answer_formulas[i], f"Expected {answer_formulas[i][0]} and {answer_formulas[i][1]}, got {result[0]} and {result[1]}"
+    print("PASS")
+
